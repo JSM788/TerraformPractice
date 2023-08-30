411084195a49823d21cc7939ed5c2cb1be1d1645
Juan Salinas Misayauri <98189391+JSM788@users.noreply.github.com>
2023-08-29 22:43:32 -0500

    Update bootstrap.sh
    adding new script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,16 +4,17 @@ sudo apt update
 #innstall apache2
 sudo apt -y install apache2 git rsync
 #create index.html
-sudo bash -c 'echo "texto de prueba" > /var/www/html/index.html'
+sudo bash -c 'echo "bonpland app dev" > /var/www/html/index.html'
 #install node
 cd ~
 curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt -y install nodejs
-# Crea archivo id_rsa
-su - ubuntu -c "chmod 400 /home/ubuntu/.ssh/id_rsa"
-# Add GitHub's public key to known_hosts
-su - ubuntu -c "ssh-keyscan github.com >> /home/ubuntu/.ssh/known_hosts"
-#install front Bonpland
-su - ubuntu -c "mkdir /home/ubuntu/tmp && cd /home/ubuntu/tmp && git clone git@github.com:Inmobiliaria-Bonpland/frontend.git && cd frontend && git checkout dev && npm install && npm run build"
-sudo rsync -av /home/ubuntu/tmp/frontend/dist/ /var/www/html/
+#install pokemon app
+mkdir /home/ubuntu/tmp
+cd /home/ubuntu/tmp
+git clone https://github.com/Megh2507/Pokemon-App.git
+cd /home/ubuntu/tmp/Pokemon-App
+npm install
+npm run build
+sudo rsync -av /home/ubuntu/tmp/Pokemon-App/build/ /var/www/html/
